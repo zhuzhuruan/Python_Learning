bf3d0e69c4f44f15105850ba14a880582decb931
zhuzhuruan <yyuan_cao@163.com>
2021-07-28 18:02:48 +0800

    普通时区计数脚本

--- 3_利用Python进行数据分析/第一章_引言.py
+++ 3_利用Python进行数据分析/第一章_引言.py
@@ -4,4 +4,12 @@ import json
 file_path = '源文件数据/1_引言/bitly_usagov.txt'
 with open(file_path, 'rt', encoding='utf-8') as f:
     records = [json.loads(line) for line in f]  # 列表推导式
-print(records[0], type(records))
+
+tz_dic = {}
+for item in records:
+    if 'tz' in list(item.keys()):
+        tz_dic[item['tz']] = tz_dic.get(item['tz'], 0) + 1
+item = list(tz_dic.items())
+item.sort(key=lambda x: x[1], reverse=True)
+print(item)
+
