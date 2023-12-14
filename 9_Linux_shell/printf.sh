8beda183c0d554fa635acc407b6c294fc3026749
zhuzhuruan <yyuan_cao@163.com>
2023-12-14 17:53:55 +0800

    20231214

---
+++ 9_Linux_shell/printf.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# 文件名：printf.sh
+
+printf "%-5s	%-10s	%-4s\n"	No	Name	Mark
+printf "%-5s	%-10s	%-4s\n"	1	Sarath	85.6920
+printf "%-5s	%-10s	%-4s\n"	2	James	90.5728
+printf "%-5s	%-10s	%-4s\n"	3	Jeff	77.5821
